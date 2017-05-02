552b77015fc39577115fb8ab300dfdc4ad5dd6b9
szrharrison <s.z.r.harrison@gmail.com>
2017-05-02 10:14:27 -0400

    Done

--- js/init.js
+++ js/init.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) {
-  var API_KEY = "GO GET AN API KEY";
+  var API_KEY = "df5e6d0a8cb4a375";
   var URL = "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
   var ctx = $("#NYCWeatherChart").get(0).getContext("2d");
 
--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,41 @@
 function getFahrenheits(result){
   // Your code goes here
+  let temps = result.hourly_forecast.map(function(forecast, i, hourly_forecast) {
+    return forecast.temp.english
+  })
+  return temps
 }
 
 function getHours(result){
   // Your code goes here
+  let hours = result.hourly_forecast.map(function(forecast, i, hourly_forecast) {
+    return forecast.FCTTIME.hour_padded
+  })
+  return hours
 }
 
 function generateDataSet(labels, data) {
   // Your code goes here
+  return {
+    labels: labels,
+    datasets: [{
+      data: data,
+      label : 'Hourly Weather for New York',
+      fillColor : 'rgba(220,220,220,0.2)',
+      strokeColor : 'rgba(220,220,220,1)',
+      pointColor : 'rgba(220,220,220,1)',
+      pointStrokeColor : '#fff',
+      pointHighlightFill : '#fff',
+      pointHighlightStroke : 'rgba(220,220,220,1)',
+    }],
+  }
 }
 
 function makeAjaxRequest(endpoint, success) {
   // Your code goes here
+  $.ajax({
+    url: endpoint,
+    dataType : 'jsonp',
+    success: success
+  })
 }
